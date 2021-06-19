70ad6310c7ab56e91b0c17df02f52561dd309e70
Michael <mfw6mw@virginia.edu>
2021-06-19 17:42:13 -0400

    Final Add before video demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
+
 Car.create(
     year: 2021,
     make: "Subaru",
     model: "Outback",
     color: "Grey",
-    image: "https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349",
-    vin: "V123497643223"
+    image: "https://cdn3.iconfinder.com/data/icons/car-icons-front-views/480/Generic_Car_Front_View-512.png",
+    vin: "4Y1SL65848Z411439"
   )
 Car.create(
     year: 2007,
     make: "Honda",
     model: "Accord",
     color: "Blue",
-    image: "https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349",
+    image: "https://cdn3.iconfinder.com/data/icons/car-icons-front-views/480/Generic_Car_Front_View-512.png",
     vin: "VHDNIW837GRUBFRY64"
   )
   Car.create(
@@ -27,16 +30,16 @@ Car.create(
     make: "Toyota",
     model: "Corolla",
     color: "White",
-    image: "https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349",
-    vin: "95VBDJKDHJDB7"
+    image: "https://cdn3.iconfinder.com/data/icons/car-icons-front-views/480/Generic_Car_Front_View-512.png",
+    vin: "95VBDJKDHJDB79H63"
   )
   Car.create(
     year: 2015,
     make: "Toyota",
     model: "Rav 4",
     color: "Grey",
-    image: "https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349",
-    vin: "97FJD74NGMGI63YBNFKOG0"
+    image: "https://cdn3.iconfinder.com/data/icons/car-icons-front-views/480/Generic_Car_Front_View-512.png",
+    vin: "97FJD74NGMGI63YBNF"
   )
 
 
@@ -50,24 +53,54 @@ Record.create(
     service: "Tire Rotation",
     car_id: rand(1..4)
   )
-  Record.create(
+Record.create(
     mileage: rand(0...300000),
     service: "Brake Replacement",
     car_id: rand(1..4)
   )
-  Record.create(
+Record.create(
     mileage: rand(0...300000),
     service: "Rotor Replacement",
     car_id: rand(1..4)
   )
-  Record.create(
+Record.create(
     mileage: rand(0...300000),
-    service: "Oil Change",
+    service: "Transmission Replacement",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0...300000),
+    service: "Tire Rotation",
     car_id: rand(1..4)
   )
   Record.create(
-    mileage: rand(0...300000),
+    mileage: rand(0..300000),
     service: "Oil Change",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0..300000),
+    service: "Tire Rotation",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0...300000),
+    service: "Brake Replacement",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0...300000),
+    service: "Rotor Replacement",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0...300000),
+    service: "Transmission Replacement",
+    car_id: rand(1..4)
+  )
+Record.create(
+    mileage: rand(0...300000),
+    service: "Tire Rotation",
     car_id: rand(1..4)
   )
 
